725f6421c12eb6d7925c8c64b6d101d309e40a8f
Haruhiko Takada <haruhiko.takada@kyash.co>
2023-08-18 18:23:00 +0900

    テーブルへのInsertができた。

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,20 @@
-import com.google.cloud.bigquery.*
-import java.util.*
+import com.google.cloud.Timestamp
+import com.google.cloud.bigquery.BigQueryOptions
+import com.google.cloud.bigquery.InsertAllRequest
 
-
-fun main(args: Array<String>) {
-    val bigquery = BigQueryOptions.getDefaultInstance().getService()
-    val queryConfig = QueryJobConfiguration.newBuilder(
-        "SELECT CONCAT('https://stackoverflow.com/questions/', "
-                + "CAST(id as STRING)) as url, view_count "
-                + "FROM `bigquery-public-data.stackoverflow.posts_questions` "
-                + "WHERE tags like '%google-bigquery%' "
-                + "ORDER BY view_count DESC "
-                + "LIMIT 10"
-    ).setUseLegacySql(false).build()
-    val jobId = JobId.of(UUID.randomUUID().toString())
-    var queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build())
-
-    queryJob = queryJob.waitFor()
-
-    if (queryJob == null) {
-        throw RuntimeException("Job no longer exists")
-    } else if (queryJob.status.error != null) {
-        throw RuntimeException(queryJob.status.error.toString())
-    }
-    val result: TableResult = queryJob.getQueryResults()
-    for (row: FieldValueList in result.iterateAll()) {
-        val url = row["url"].getStringValue()
-        val viewCount = row["view_count"].getStringValue()
-        System.out.printf("%s : %s views\n", url, viewCount)
+const val PROJECT_ID = "your_project_id"
+const val DATASET_NAME = "mobile_develop"
+const val TABLE_NAME = "build_time_test"
+fun main() {
+    val bigquery = BigQueryOptions.newBuilder().setProjectId(PROJECT_ID).build().service
+    val timestamp = Timestamp.now().toString()
+    val duration = 12345L
+    val content = mapOf<String, Any>("created_at" to timestamp, "duration" to duration)
+    val request = InsertAllRequest.newBuilder(DATASET_NAME, TABLE_NAME).addRow(content).build()
+    val response= bigquery.insertAll(request)
+    if (response.hasErrors()) {
+        throw RuntimeException(response.insertErrors.toString())
+    } else {
+        println("Success")
     }
 }
